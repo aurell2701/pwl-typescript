f7ac0168318becaa3f40d18a79d49d8103fdf894
aurell2701 <h1101241043@student.untan.ac.id>
2026-02-25 04:46:17 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function hello(nama: string, umur: number): string {
     return `Selamat Datang ${nama} di Pontianak, umur kamu ${umur} tahun`;
 }
 
-const mahasiswa = "Andi";
+const mahasiswa = "Aurell";
 const umur: number = 19;
 
 console.log(hello(mahasiswa, umur));
