1275ca8b8572b959366130927727ca324405a608
aurell2701 <h1101241043@student.untan.ac.id>
2026-02-25 04:42:40 +0000

    update index

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function hello(nama: string, umur: number): string {
+    return `Selamat Datang ${nama} di Pontianak, umur kamu ${umur} tahun`;
+}
+
+const mahasiswa = "Andi";
+const umur: number = 19;
+
+console.log(hello(mahasiswa, umur));
